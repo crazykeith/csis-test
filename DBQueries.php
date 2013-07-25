ef0b453c788e01b3e77c76c43f74314d97cb491a
crazykeith <shaggysbc69@gmail.com>
2013-07-25 19:44:13 -0400

    Update DBQueries.php

--- DBQueries.php
+++ DBQueries.php
@@ -1,28 +1,29 @@
 class DBQueries
 {
-  protected $num_results = 0;
-
+    protected $num_results = 0;
 
 	protected function selectQuery($sql,$search) {
-
-        /* DBQueries->selectQuery($sql,$search)
+	
+	/* DBQueries->selectQuery($sql,$search)
 		* Description:
 		*   Base function for selected data from the database.
 		* Parameters:
-		*   $sql - a string containing the SQL statement customized for whatever purpose.
-        *   $search - a keyed array containing all of the named parameters for the SQL statement
+		*   $sql - a string containing the SQL statement
+		*			customized for whatever purpose.
+		*   $search - a keyed array containing all of the
+		*			named parameters for the SQL statement
 		* Sample Usage:
 		*   $DB_class = new DBQueries;
 		*	$item_array = $DB_class->selectQuery("SELECT * FROM Buildings","");
 		* Return Argument:
 		*   returns a keyed array of the database query results.
 		*/
-
+	
 		$conn = new DBH();
 		$DBH = $conn->getConnection();
 		$result = array();
 		$i = 0;
-
+	
 		try {
 			$STH = $DBH->prepare($sql);
 			$STH->setFetchMode(PDO::FETCH_ASSOC);
@@ -46,24 +47,27 @@ class DBQueries
 		}
 	}
 	protected function insertUpdateDeleteQuery($sql,$changes) {
-
-        /* DBQueries->insertUpdateDeleteQuery($sql,$changes)
+	
+	/* DBQueries->insertUpdateDeleteQuery($sql,$changes)
 		* Description:
-		*   Base function for inserting, updating, or deleting data from the database.
+		*   Base function for inserting, updating, or
+		*	deleting data from the database.
 		* Parameters:
-		*   $sql - a string containing the SQL statement customized for whatever purpose.
-        *   $changes - a keyed array containing all of the named parameters for the SQL statement
+		*   $sql - a string containing the SQL statement
+		*		customized for whatever purpose.
+		*   $changes - a keyed array containing all of the named
+		*			parameters for the SQL statement
 		* Sample Usage:
 		*   $DB_class = new DBQueries;
 		*	$item_array = $DB_class->insertUpdateDeleteQuery($sql,$changes);
 		* Return Argument:
 		*   returns a bool value of the success of the performed query.
 		*/
-
+	
 		$conn = new DBH();
 		$DBH = $conn->getConnection();
 		$result = false;
-
+	
 		try {
 			$STH = $DBH->prepare($sql);
 			$STH->setFetchMode(PDO::FETCH_ASSOC);
@@ -73,7 +77,7 @@ class DBQueries
 			} else {
 				$result = false;
 			}
-
+	
 			return $result;
 		} catch (PDOException $e) {
 			$message = $e->getMessage();
