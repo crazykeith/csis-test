10a2094df182cfaa6ec5196e7333039450359d8c
crazykeith <shaggysbc69@gmail.com>
2013-07-25 18:58:02 -0400

    Create ticketfunctions.js

---
+++ ticketfunctions.js
@@ -0,0 +1,64 @@
+function getActiveTickets() {
+    $.post("<?php echo sanitizeHTML(BASE_URL);?>/tickets/ticketAJAX.php",{
+        type: "get_active_ticket_count"
+    } , function (data) {
+        var tickets = $("#recent-posts").children('h4').size();
+        if (data != tickets) {
+            location.reload();
+        }
+    })
+    setTimeout(arguments.callee, 30000);
+}
+function getTicketInfo(ticket_id) {
+    $("input[type=radio],input[type=checkbox]").prop('checked',false);
+    $("#appt").hide();
+    $("#apptDate").val('');
+    $("#apptTime").val('');
+    $.getJSON("<?php echo sanitizeHTML(BASE_URL);?>/tickets/ticketAJAX.php?ticket_id="
+            +ticket_id+"&type=get_ticket",
+        function(json){
+            var i=0;
+            var edit_priority_tag = '#edit-priority-'+json.priority;
+            var edit_addon_tag = '#edit-addon-'+json.addon;
+            var category_length = json.categories.length;
+            while (i < category_length) {
+                $("#cat_"+json.categories[i].category_id).prop('checked',true);
+                i++;
+            }
+            $( "#edit-ticket-textarea").html(json.description);
+            $(edit_priority_tag).prop('checked',true);
+            $(edit_addon_tag).prop('checked',true);
+            if (json.addon == "Appointment") {
+                $("#appt").show('bounce',1000);
+                var appt_array = json.appt.split(" ");
+                $("#apptDate").val(appt_array[0]);
+                $("#apptTime").val(appt_array[1]);
+                $("#appt"+appt_array[2]).prop('checked', true);
+            } else {
+                $("#apptPM").prop('checked', true);
+            }
+            $(".radio").buttonset("refresh");
+        })
+}
+function getIssuerInfo(ticket_id) {
+    $.getJSON("<?php echo sanitizeHTML(BASE_URL);?>/tickets/ticketAJAX.php?ticket_id="
+        +ticket_id+"&type=get_ticket",
+        function(json){
+            if (json.issuer_first_name != null && json.issuer_last_name != null) {
+                var full_name = json.issuer_first_name+' '+json.issuer_last_name;
+            } else {
+                var full_name = null;
+            }
+            var email = '<a href="mailto:'+json.email+'">'+json.email+'</a>';
+            $("#edit-issuer-ticket-id").val(json.id);
+            $("#issuer-gatorlink-"+ticket_id).html(json.issuer_gatorlink);
+            $("#issuer-name-"+ticket_id).html(json.full_name);
+            $("#issuer-email-"+ticket_id).html(email);
+            $("#issuer-phone-"+ticket_id).html(json.phone);
+            $("#edit-issuer-gatorlink").val(json.issuer_gatorlink);
+            $("#edit-issuer-name").val(full_name);
+            $("#edit-issuer-email").val(json.issuer_email);
+            $("#edit-issuer-phone").val(json.issuer_phone);
+        })
+}
+setTimeout(getActiveTickets,30000);
